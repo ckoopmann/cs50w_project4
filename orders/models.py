2a40c7722de188db1dafa51b79a35b45c229c75a
Christian Koopmann <c.k.e.koopmann@gmail.com>
2019-06-30 13:26:57 +0200

    First version of menu

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length = 30)
+    size = models.CharField(max_length = 10)
+    type = models.CharField(max_length = 30)
+    prize = models.FloatField()
+    num_toppings = models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.id} - {self.type}, {self.name}, {self.size}, {self.prize}"
+
+class Topping(models.Model):
+    name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return f"{self.id} - {self.name}"
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import MenuItem
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    context = {
+    "regular_pizzas": MenuItem.objects.filter(type = "Regular Pizza"),
+    "sicilian_pizzas": MenuItem.objects.filter(type = "Sicilian Pizza")
+    }
+    return render(request, "orders/index.html", context)
