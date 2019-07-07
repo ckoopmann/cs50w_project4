cd0f5a270ebdcdd55517561ded19c3263d94a55e
Christian Koopmann <c.k.e.koopmann@gmail.com>
2019-07-07 16:13:31 +0200

    Restructuring Menu Page

--- orders/views.py
+++ orders/views.py
@@ -4,8 +4,21 @@ from .models import MenuItem
 
 # Create your views here.
 def index(request):
-    context = {
-    "regular_pizzas": MenuItem.objects.filter(type = "Regular Pizza"),
-    "sicilian_pizzas": MenuItem.objects.filter(type = "Sicilian Pizza")
-    }
+
+    types  = MenuItem.objects.order_by().values_list('type', flat = True).distinct()
+
+    context = {}
+
+    for type in types:
+        values = {}
+
+        names = MenuItem.objects.filter(type = type).values_list('name', flat = True).distinct()
+
+        for name in names:
+            values[name] = MenuItem.objects.filter(type = type, name = name)
+
+        context[type.replace(" ", "")+'s'] = values
+
+    print(context)
+
     return render(request, "orders/index.html", context)
